db5440a4d8a034d7ea38db829a9244b7b6a4a91b
David de Oliveira Lima <dipias2002@gmail.com>
2020-12-07 14:06:33 -0300

    Bug Fix

--- main.py
+++ main.py
@@ -8,28 +8,69 @@ file = open('moedas.txt', 'r', encoding='UTF-8')
 moedas = file.read().replace('\n',', ')
 file.close()
 moedas = ast.literal_eval(moedas)
-
 dataAtual = str(datetime.datetime.now())
 dataAtual = dataAtual[:dataAtual.index(' ')]
 dataAtual = dataAtual[:dataAtual.rindex('-')]+str(int(dataAtual[dataAtual.rindex('-'):])+1)
 
 def conversao():
+    global valor
+    global moedaOriginal
+    global moedaDestino
     valorOriginal = valor1.get()
     moedaOriginal = moedas[moeda1.get()]
     moedaDestino = moedas[moeda2.get()]
-    url = f'https://www3.bcb.gov.br/bc_moeda/rest/converter/{valorOriginal}/1/{moedaOriginal}/{moedaDestino}/{dataAtual}'
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
+    
+    try:
+        url = f'https://www3.bcb.gov.br/bc_moeda/rest/converter/{valorOriginal}/1/{moedaOriginal}/{moedaDestino}/{dataAtual}'
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        valor = soup.find('valor-convertido')
 
-    valor = soup.find('valor-convertido').text
+        if valor == None:
+            if atualizarCotacao():
+                url = f'https://www3.bcb.gov.br/bc_moeda/rest/converter/{valorOriginal}/1/{moedaOriginal}/{moedaDestino}/{dataAtual}'
+                req = requests.get(url)
+                soup = BeautifulSoup(req.text, 'html.parser')
+                valor = soup.find('valor-convertido')
 
-    print(valor)
-    valor2.set(valor)
+        valor = soup.find('valor-convertido').text
+        valor2.set(valor)
+        cotacao.setvar(dataAtual)
+
+        print(">>> Conversão bem-sucedida")
+        return True
+
+    except Exception as E:
+        print(">>> Erro na conversão")
+        print(">>> "+ str(E))
+        return False
 
 def inversao():
     temp = moeda1.get()
     moeda1.set(moeda2.get())
     moeda2.set(temp)
+
+def atualizarCotacao():
+    global dataAtual
+    global valor
+    window.title('Carregando...')
+
+    while valor == None:
+            url = f'https://www3.bcb.gov.br/bc_moeda/rest/converter/1/1/{moedaOriginal}/{moedaDestino}/{dataAtual}'
+            req = requests.get(url)
+            soup = BeautifulSoup(req.text, 'html.parser')
+            valor = soup.find('valor-convertido')
+            if valor != None:
+                break
+            else:
+                if int(dataAtual[dataAtual.rindex('-')+1:]) != 1:
+                    dataAtual = dataAtual[:dataAtual.rindex('-')]+str(int(dataAtual[dataAtual.rindex('-'):])+1)
+                else:
+                    dataAtual = dataAtual[:dataAtual.index('-')]+'-'+str(int(dataAtual[dataAtual.index('-')+1:dataAtual.rindex('-')])-1)+'-'+str(30)
+
+    window.title('Conversor de moedas')
+    cotacao.config(text='Cotação de:\n'+dataAtual.replace('-','/'))
+    return True
 
 if __name__ == "__main__":    
     window = Tk()
@@ -61,8 +102,14 @@ if __name__ == "__main__":
     moeda2dropdown = OptionMenu(window,moeda2, *moedas)
     moeda2dropdown.grid(row=1, column=3)
 
+    cotacaoText = ''
+    cotacao = Label(window, text='Cotação de:\n'+cotacaoText)
+    cotacao.grid(row=3,column=0)
+    cotacao.config(text='Cotação de:\n'+dataAtual.replace('-','/'))
+    
     conversao()
-    botaoConverter = Button(window, text='Converter',command=conversao)
+    
+    botaoConverter = Button(window, text='Converter', command=conversao)
     botaoConverter.grid(row=3,column=1)
 
     botaoInverter = Button(window, text='Inverter Moedas', command=inversao)
